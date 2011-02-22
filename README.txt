4680f42a5f36ab47f79b8c18add47dd15be9c008
Dither <Dither@users.noreply.github.com>
2011-02-22 13:42:18 +0300

    First version

---
+++ README.txt
@@ -0,0 +1,8 @@
+﻿Save snapshot of the page for Opera
+=============
+
+Extension that saves snapshot of the pages including all modifications made by scripts.
+
+For correct saving create «text/phf» MIME type (Settings->Preferences->Downloads->Add)
+with «htm» extension and "Save to disk" action.
+ 
---
+++ includes/background.js
@@ -0,0 +1,186 @@
+// ==UserScript==
+// @include http://*
+// @include https://*
+// ==/UserScript==
+
+(function(){
+var bImages, bB64enc, bDebug;
+
+var dataEncodeImg = function (img) {
+        var canvas = window.document.getElementById('tmpcanvas');
+        if (!canvas) canvas = window.document.createElement('canvas');
+        canvas.id = 'tmpcanvas';
+        
+        var context = canvas.getContext('2d');
+        canvas.width = img.width;
+        canvas.height = img.height;
+        context.drawImage(img, 0, 0);
+        
+        var result = null;
+        try { result = canvas.toDataURL(); } 
+        catch (ex) { //no cross-domain images allowed
+        }
+        return result;
+}
+    
+var getsnapshot = function () {
+    var encodeBase64 = function (a) {
+        if (!bB64enc) return a;
+        var b, c, d, e = '',
+            f = [],
+            i = 0,
+            j = 0,
+            g = 0,
+            h = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/='.split('');
+        while (g = a.charCodeAt(j++)) {
+            if (g < 128) {
+                f[f.length] = g
+            } else if (g < 2048) {
+                f[f.length] = 192 | (g >> 6);
+                f[f.length] = 128 | (g & 63)
+            } else if (g < 65536) {
+                f[f.length] = 224 | (g >> 12);
+                f[f.length] = 128 | ((g >> 6) & 63);
+                f[f.length] = 128 | (g & 63)
+            } else {
+                f[f.length] = 240 | (g >> 18);
+                f[f.length] = 128 | ((g >> 12) & 63);
+                f[f.length] = 128 | ((g >> 6) & 63);
+                f[f.length] = 128 | (g & 63)
+            }
+        };
+        while (i < f.length) {
+            b = f[i++];
+            c = f[i++];
+            d = f[i++];
+            e += h[b >> 2];
+            e += h[((b & 3) << 4) | (c >> 4)];
+            e += h[c === undefined ? 64 : ((c & 15) << 2) | (d >> 6)];
+            e += h[d === undefined ? 64 : d & 63]
+        };
+        return e
+    };
+    var selWin = function (w) {
+        if (w.getSelection() != '') return w;
+        for (var i = 0, f, r; f = w.frames[i]; i++) {
+            try {
+                if (r = arguments.callee(f)) return r
+            } catch (e) {}
+        }
+    };
+    var ele, pEle, clone;
+    var doctype = '';
+    var doc = window.document;
+    var loc = window.location;
+    var win = selWin(window);
+    var domain=loc.protocol+"//"+loc.host+(loc.port!=''?':'+loc.port:'');
+    
+    if (win) {
+        doc = win.document;
+        loc = win.location;
+        var s = win.getSelection();
+        var r = s.getRangeAt(0);
+        pEle = r.commonAncestorContainer;
+        ele = r.cloneContents()
+    } else {
+        pEle = doc.documentElement;
+        ele = (doc.body || doc.getElementsByTagName('body')[0]).cloneNode(true)
+    };
+
+    while (pEle) {
+        if (pEle.nodeType == 1) {
+            clone = pEle.cloneNode(false);
+            clone.appendChild(ele);
+            ele = clone
+        };
+        pEle = pEle.parentNode
+    };
+        
+    var sel = doc.createElement('div');
+    sel.appendChild(ele);
+    
+    if (bImages) {
+	    var imgcache=[], images = sel.getElementsByTagName('img');
+	    for (var i = images.length; i--;) {
+	    	var imgsrc = images[i].src;
+	    	if (imgsrc == null) continue;
+	    	if (domain == imgsrc.substring(0, domain.length))
+	    	{
+		    	if (imgcache[imgsrc] != null) images[i].src = imgcache[imgsrc];
+		        else {
+		        	imgcache[imgsrc] = dataEncodeImg(images[i]);
+		        	images[i].src = imgcache[imgsrc];
+		        }
+		}
+	    };
+	    imgcache = null;
+    }
+    
+    var scripts = sel.getElementsByTagName('script');
+    for (var i = scripts.length; i--;) {
+        scripts[i].parentNode.removeChild(scripts[i])
+    };
+      
+    var h = ele.insertBefore(doc.createElement('head'), ele.firstChild);
+    var title = doc.getElementsByTagName('title')[0];
+    title = title ? title.text : 'untitled';
+    var link = loc.href;
+    
+    var t = doc.createElement('title');
+    t.text = title;
+    h.appendChild(t);
+    
+    var meta = doc.createElement('meta');
+    meta.httpEquiv = 'content-type';
+    meta.content = 'text/html; charset=utf-8';
+    h.appendChild(meta);
+    
+    var base = doc.getElementsByTagName('base')[0];
+    var b = base ? base.cloneNode(false) : doc.createElement('base');
+    if (!b.href) b.href = link;
+    h.appendChild(b);
+    
+    var styles = doc.styleSheets;
+    for (var i = 0, si; si = styles[i]; i++) {
+        var style = doc.createElement('style');
+        style.type = 'text/css';
+        if (si.media.mediaText) style.media = si.media.mediaText;
+        try {
+            for (var j = 0, rule; rule = si.cssRules[j]; j++) {
+                style.appendChild(doc.createTextNode(rule.cssText + '\n'))
+            }
+        } catch (e) {
+            if (si.ownerNode) style = si.ownerNode.cloneNode(false)
+        };
+        h.appendChild(style)
+    };
+    
+    var dt = doc.doctype;
+    if (dt && dt.name) {
+        doctype += '<!DOCTYPE ' + dt.name;
+        if (dt.publicId) doctype += ' PUBLIC \x22' + dt.publicId + '\x22';
+        if (dt.systemId) doctype += ' \x22' + dt.systemId + '\x22';
+        doctype += '>\n'
+    };
+    return 'data:text/phf;charset=UTF-8;base64,' + encodeBase64(doctype + sel.innerHTML + '\n<!-- Saved from: ' + link + ' -->');
+}
+
+function log() {
+	if (!bDebug) return;
+	if (window.console) console.log(Array.prototype.slice.call(arguments));
+	else if (window.opera && window.opera.postError) window.opera.postError(arguments);
+}
+
+window.addEventListener('load', function(){
+	if(opera.extension) opera.extension.onmessage = function(e){ switch (e.data.type){
+	case 'save-snapshot':
+	case 'save-snapshot-encode':
+		if (window.location.href.indexOf(e.data.url) == -1) break; //don't know why, but opera returns incomplete url for tabs
+		bImages = e.data.images;
+		bB64enc = e.data.b64;
+		bDebug = e.data.debug;
+		opera.extension.postMessage({type: 'got-url', url: getsnapshot()});
+		break;
+	}}
+},false);
+})();
