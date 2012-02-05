13db878cb98f62f0c0211e574fa51d25c56d3055
Dither <Dither@users.noreply.github.com>
2012-02-05 12:53:25 +0400

    Notify-it support.

---
+++ README.md
@@ -0,0 +1,20 @@
+# Save snapshot of the page for Opera
+=============
+
+Extension saves page source code and CSS styles including all modifications made by scripts and Opera.
+
+For correct saving create «text/phf» MIME type (Settings-Preferences-Advanced-Downloads-Add) with «htm» extension and «Save to disk» action. This extension will not save page's JavaScripts.
+
+Changelog:
+v1.4:
+* N
+v1.32:
+* fix for null img src's;
+* button automatically disabled for blank pages.
+* fixed multiple save dialogs if several tabs of the same url opened;
+v1.31:
+* fixed loading on some sites;
+* actual debug info;
+v1.3:
+* fix for "null" image src's;
+* button automatically disabled for blank pages;
--- includes/save-snapshot.js
+++ includes/save-snapshot.js
@@ -5,26 +5,26 @@
 
 (function(){
 var bImages = false, bB64enc = true, bDebug = false;
-
 function log(){ if (bDebug) opera.postError('[SaveSnapshot]: ' + Array.prototype.slice.call(arguments)); }
-
-var dataEncodeImg = function (img) {
-        var canvas = window.document.getElementById('tmpcanvas');
-        if (!canvas) canvas = window.document.createElement('canvas');
-        canvas.id = 'tmpcanvas';
-        
-        var context = canvas.getContext('2d');
-        canvas.width = img.width;
-        canvas.height = img.height;
-        context.drawImage(img, 0, 0);
-        
-        var result = null;
-        try { result = canvas.toDataURL(); } 
-        catch (bugs) {;} //no cross-domain images allowed
-        return result;
-}
     
 var getsnapshot = function () {
+    var uriEncodeImg = function (img) {
+            var canvas = window.document.getElementById('tmpcanvas');
+            if (!canvas) canvas = window.document.createElement('canvas');
+            canvas.id = 'tmpcanvas';
+            
+            var context = canvas.getContext('2d');
+            canvas.width = img.width;
+            canvas.height = img.height;
+            context.drawImage(img, 0, 0);
+            
+            var result = null;
+            try { result = canvas.toDataURL(); } 
+            catch (bug) { //no cross-domain images allowed
+                log(img.src + ' is crossdomain or don\'t come with access-control-allow for CORS.' );
+            }
+            return result;
+    };
     var encodeBase64 = function (a) {
         if (!bB64enc) return a;
         else log('encoding page content as base64...');
@@ -36,19 +36,19 @@ var getsnapshot = function () {
             h = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/='.split('');
         while (g = a.charCodeAt(j++)) {
             if (g < 128) {
-                f[f.length] = g
+                f[f.length] = g;
             } else if (g < 2048) {
                 f[f.length] = 192 | (g >> 6);
-                f[f.length] = 128 | (g & 63)
+                f[f.length] = 128 | (g & 63);
             } else if (g < 65536) {
                 f[f.length] = 224 | (g >> 12);
                 f[f.length] = 128 | ((g >> 6) & 63);
-                f[f.length] = 128 | (g & 63)
+                f[f.length] = 128 | (g & 63);
             } else {
                 f[f.length] = 240 | (g >> 18);
                 f[f.length] = 128 | ((g >> 12) & 63);
                 f[f.length] = 128 | ((g >> 6) & 63);
-                f[f.length] = 128 | (g & 63)
+                f[f.length] = 128 | (g & 63);
             }
         };
         while (i < f.length) {
@@ -58,14 +58,14 @@ var getsnapshot = function () {
             e += h[b >> 2];
             e += h[((b & 3) << 4) | (c >> 4)];
             e += h[c === undefined ? 64 : ((c & 15) << 2) | (d >> 6)];
-            e += h[d === undefined ? 64 : d & 63]
-        };
-        return e
+            e += h[d === undefined ? 64 : d & 63];
+        }
+        return e;
     };
     var selWin = function (w) {
         if (w.getSelection() != '') return w;
         for (var i = 0, f, r; f = w.frames[i]; i++) {
-            try { if (r = arguments.callee(f)) return r; } catch (e) {;}
+            try { if (r = arguments.callee(f)) return r; } catch (bug) {}
         }
     };
     
@@ -80,51 +80,55 @@ var getsnapshot = function () {
         doc = win.document;
         loc = win.location;
         var s = win.getSelection();
-        var r = s.getRangeAt(0);
-        pEle = r.commonAncestorContainer;
-        ele = r.cloneContents()
+        if (s) {
+            var r = s.getRangeAt(0);
+            pEle = r.commonAncestorContainer;
+            ele = r.cloneContents();
+        } else {
+            log('error, expected selection got null');
+        }
     } else {
         pEle = doc.documentElement;
-        ele = (doc.body || doc.getElementsByTagName('body')[0]).cloneNode(true)
-    };
+        ele = (doc.body || doc.getElementsByTagName('body')[0]).cloneNode(true);
+    }
 
     log('cloning document...');
     while (pEle) {
-        if (pEle.nodeType == 1) {
+        if (pEle.nodeType === 1 /* Node.DOCUMENT_POSITION_DISCONNECTED */) {
             clone = pEle.cloneNode(false);
             clone.appendChild(ele);
-            ele = clone
-        };
-        pEle = pEle.parentNode
+            ele = clone;
+        }
+        pEle = pEle.parentNode;
     };
         
     var sel = doc.createElement('div');
     sel.appendChild(ele);
     
-    log('parsing images = '+bImages);
+    log('parsing images = ' + bImages);
     if (bImages) {
-	    var imgcache=[], images = sel.getElementsByTagName('img');
-	    for (var i = images.length; i--;) {
-	    	var imgsrc = images[i].src;
-	    	if (imgsrc == null) continue;
-	    	if (domain == imgsrc.substring(0, domain.length))
-	    	{
-	    		if(bDebug) log('parsing image from '+imgsrc);
-		    	if (imgcache[imgsrc]) images[i].src = imgcache[imgsrc];
-		        else {
-		        	if (imgcache[imgsrc] = dataEncodeImg(images[i])) images[i].src = imgcache[imgsrc];
-		        	if(bDebug) log('img cached and converted to '+imgcache[imgsrc].substring(0,60)+'...');
-		        }
-		}
-	    };
-	    imgcache = null;
+        var imgcache = [], logarray = [], images = sel.getElementsByTagName('img');
+        for (var i = images.length; i--;) {
+            var imgsrc = images[i].src;
+            if (imgsrc === null) continue;
+            if (domain == imgsrc.substring(0, domain.length)) {
+                if (bDebug) logarray.push('parsing image ' + imgsrc);
+                if (imgcache[imgsrc]) images[i].src = imgcache[imgsrc];
+                else {
+                    if (imgcache[imgsrc] = uriEncodeImg(images[i])) images[i].src = imgcache[imgsrc];
+                    if (bDebug) logarray.push('img cached and converted to ' + imgcache[imgsrc].substring(0,70)+'...');
+                }
+            }
+        };
+        log(logarray.length && logarray.join('\n'));
+        imgcache = null;
     }
     
     log('removing scripts...');
     var scripts = sel.getElementsByTagName('script');
     for (var i = scripts.length; i--;) {
-        scripts[i].parentNode.removeChild(scripts[i])
-    };
+        scripts[i].parentNode.removeChild(scripts[i]);
+    }
       
     var h = ele.insertBefore(doc.createElement('head'), ele.firstChild);
     var title = doc.getElementsByTagName('title')[0];
@@ -153,12 +157,12 @@ var getsnapshot = function () {
         if (si.media.mediaText) style.media = si.media.mediaText;
         try {
             for (var j = 0, rule; rule = si.cssRules[j]; j++) {
-                style.appendChild(doc.createTextNode(rule.cssText + '\n'))
+                style.appendChild(doc.createTextNode(rule.cssText + '\n'));
             }
-        } catch (e) {
-            if (si.ownerNode) style = si.ownerNode.cloneNode(false)
-        };
-        h.appendChild(style)
+        } catch (bug) {
+            if (si.ownerNode) style = si.ownerNode.cloneNode(false);
+        }
+        h.appendChild(style);
     };
     
     var dt = doc.doctype;
@@ -166,25 +170,46 @@ var getsnapshot = function () {
         doctype += '<!DOCTYPE ' + dt.name;
         if (dt.publicId) doctype += ' PUBLIC \x22' + dt.publicId + '\x22';
         if (dt.systemId) doctype += ' \x22' + dt.systemId + '\x22';
-        doctype += '>\n'
-    };
+        doctype += '>\n';
+    }
     
     log('page snapshot successfuly created.');
     return 'data:text/phf;charset=UTF-8;base64,' + encodeBase64(doctype + sel.innerHTML + '\n<!-- Saved from: ' + link + ' @ ' + Date() + ' -->');
-}
+};
 
-document.addEventListener('DOMContentLoaded', function () {
-	if(opera.extension) opera.extension.onmessage = function(e){ switch (e.data.type){
-	case 'save-snapshot':
-	case 'save-snapshot-encode':
-		bDebug = e.data.debug;
-		log('got "'+e.data.type+'" message for url='+e.data.url);
-		if (window.location.href.indexOf(e.data.url) == -1) break; //don't know why, but opera returns incomplete url for tabs
-		bImages = e.data.images;
-		bB64enc = e.data.b64;
-		log('e.source '+(!!e.source ? 'exists' : 'not exists')+'.');
-		if (e.source) e.source.postMessage({type: 'got-url', url: getsnapshot()});
-		break;
-	}}
-},false);
+document.addEventListener('DOMContentLoaded', function() {
+if(opera.extension)
+    opera.extension.onmessage = function(e) {
+        switch (e.data.type) {
+            case 'save-snapshot':
+            case 'save-snapshot-encode':
+                bDebug = e.data.debug;
+                log('got "' + e.data.type + '" message for url=' + e.data.url);
+                if(window.location.href.indexOf(e.data.url) == -1) break;
+                //don't know why, but opera returns incomplete urls for tabs sometimes
+                bImages = e.data.images;
+                bB64enc = e.data.b64;
+
+                if(e.source) {
+                    e.source.postMessage({
+                        type: 'got-url',
+                        url: getsnapshot()
+                    });
+                    ///////////////////
+                    var noe = document.createEvent('CustomEvent');
+                    noe.initCustomEvent('Notify.It', false, false, {
+                        extension: 'save-snapshot',
+                        text: 'Page snapshot created.',
+                        type: '',
+                    });
+                    document.dispatchEvent(noe);
+                    ///////////////////
+                } else {
+                    log('e.source not exist.');
+                }
+                break;
+        }
+    };
+}, false);
+
 })();
